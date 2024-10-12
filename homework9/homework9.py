72efbae9c2f4bc1126368349d81c6b67e2994429
4udofruct1k <4udofructik@gmail.com>
2024-10-12 14:26:46 +0500

    Add files via upload

--- homework9/homework9.py
+++ homework9/homework9.py
@@ -1,6 +1,13 @@
 import re
+
 milo = input("Введите почту:")
 print(f"Почта пользователя: {milo}")
 razb = re.search(r'([\w\.\-\_\:]+)\@([\w\.]+)', milo)
-print(f"Username: {razb[1]}")
-print(f"Domain: {razb[2]}")
+check = ["", ""]
+if "@" in milo:
+    check[0],check[1] = milo.split("@")
+if check[0] and check[1]:
+    print(f"Username: {razb[1]}")
+    print(f"Domain: {razb[2]}")
+else:
+    print("Некорректный email")
